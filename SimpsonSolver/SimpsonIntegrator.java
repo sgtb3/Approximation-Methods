a21e488c3a8bf8f3d81f07d2c172e6365ea3516d
sgtb3 <bnb01112@gmail.com>
2015-09-04 15:52:10 -0400

    cleaned up SimpsonSolver aligning issues

--- SimpsonSolver/SimpsonIntegrator.java
+++ SimpsonSolver/SimpsonIntegrator.java
@@ -9,7 +9,7 @@ public class SimpsonIntegrator {
 
 	//checks if parameters are valid.
 	public boolean checkConditions() {
-        if (n % 2 != 0) {
+		if (n % 2 != 0) {
 			n++;
 			System.out.print("The provided (n) value was not even, ");
 			System.out.println("(n) has been incremented");
@@ -27,39 +27,37 @@ public class SimpsonIntegrator {
 		return true;
 	}
 
-    /**
-     * Composite Simpson's Rule.
-     *
-     * @param a lower bound
-     * @param b upper bound
-     * @param n number of evaluations
-     * @param func user defined function class
-     */
+	/**
+    * Composite Simpson's Rule.
+    * @param a lower bound
+    * @param b upper bound
+    * @param n number of evaluations
+    * @param func user defined function class
+    */
 	public double integrate(double a, double b, double n, Function func) {
+		this.a = a;
+		this.b = b;
+		this.n = n;
+		double h = (b - a) / this.n;
 
-        this.a = a;
-        this.b = b;
-        this.n = n;
-        double h = (b - a) / this.n;
+		double dx = (a + h);
+		double x  = (a + dx);
+		double t1 = func.f(a);
+		double t4 = func.f(b);
+		double t3 = func.f(x);
+		double t2 = 0;
 
-        double dx = (a + h);
-        double x  = (a + dx);
-        double t1 = func.f(a);
-        double t4 = func.f(b);
-        double t3 = func.f(x);
-        double t2 = 0;
-
-        if (!checkConditions()) {
-            System.out.println("Invalid parameters. Recompile.");
-            System.exit(1);
-        }
-        for (int j = 1; j <= (n - 2)/2; j++) {
-            x  += dx;
-            t2 += func.f(x);
-            x  += dx;
-            t3 += func.f(x);
-        }
-        return (dx / 3) * (t1 + (2 * t2) + (4 * t3) + t4);
+		if (!checkConditions()) {
+			System.out.println("Invalid parameters. Recompile.");
+			System.exit(1);
+		}
+		for (int j = 1; j <= (n - 2)/2; j++) {
+			x  += dx;
+			t2 += func.f(x);
+			x  += dx;
+			t3 += func.f(x);
+		}
+		return (dx / 3) * (t1 + (2 * t2) + (4 * t3) + t4);
 	}
 }
 
