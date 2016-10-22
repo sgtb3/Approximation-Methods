7e1aec0b4ac026fc44e2f74fad36a89fa34e5861
BNB <bnb01112@gmail.com>
2016-10-21 20:00:31 -0400

    formatting

--- SimpsonSolver/Function.java
+++ SimpsonSolver/Function.java
@@ -1,6 +1,6 @@
 public class Function {
 
-    //Example: Normal Density Function with sigma = 1, mu = 0.
+    /* Example: Normal Density Function with sigma = 1, mu = 0. */
     public double f(double x) {
         return Math.exp(-Math.pow(x, 2) / 2) / Math.sqrt(2 * Math.PI);
     }
--- SimpsonSolver/SimpsonIntegrator.java
+++ SimpsonSolver/SimpsonIntegrator.java
@@ -5,23 +5,26 @@ public class SimpsonIntegrator {
 
     private double a;
     private double b;
-    private double n; //number of evaluations
+    private double n; /* number of evaluations */
 
-    //checks if parameters are valid.
+    /* checks if parameters are valid */
     public boolean checkConditions() {
+
         if (n % 2 != 0) {
             n++;
-            System.out.print("The provided (n) value was not even, ");
-            System.out.println("(n) has been incremented");
+            System.out.print("The provided (n) value was not even, (n) has " +
+                             "been incremented");
         }
+
         if (a > b) {
-            System.out.print("Invalid parameters. (a) must be less than (b). ");
-            System.out.println("Please check and recompile.");
+            System.out.print("Invalid parameters. (a) must be less than (b). " +
+                             "Please check and recompile.");
             return false;
         }
+
         if (n < 0) {
-            System.out.print("Invalid parameters. (n) must be greater than (0). ");
-            System.out.println("Please check and recompile.");
+            System.out.print("Invalid parameters. (n) must be greater "+
+                             "than (0). Please check and recompile.");
             return false;
         }
         return true;
@@ -29,15 +32,18 @@ public class SimpsonIntegrator {
 
     /**
     * Composite Simpson's Rule.
-    * @param a lower bound
-    * @param b upper bound
-    * @param n number of evaluations
-    * @param func user defined function class
+    *
+    * @param a    : lower bound
+    * @param b    : upper bound
+    * @param n    : number of evaluations
+    * @param func : user defined function class
     */
     public double integrate(double a, double b, double n, Function func) {
+
         this.a = a;
         this.b = b;
         this.n = n;
+
         double h = (b - a) / this.n;
 
         double dx = (a + h);
@@ -51,19 +57,14 @@ public class SimpsonIntegrator {
             System.out.println("Invalid parameters. Recompile.");
             System.exit(1);
         }
+
         for (int j = 1; j <= (n - 2)/2; j++) {
             x  += dx;
             t2 += func.f(x);
             x  += dx;
             t3 += func.f(x);
         }
+
         return (dx / 3) * (t1 + (2 * t2) + (4 * t3) + t4);
     }
 }
-
-
-
-
-
-
-
